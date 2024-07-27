9fa87974bb1b11a1574037f0c303ee5954a2e99b
habeebweeb <40900143+habeebweeb@users.noreply.github.com>
2024-07-26 22:26:07 -0700

    Add more checks for gizmo interaction
    When a gizmo is not visible there is an early return so the checks for
    gizmo interaction are never done. If that non-visible gizmo was the
    interacted gizmo, it will keep the interaction status and disallow other
    gizmos from being interacted with. Add a check in the !Visible check
    block and reset interaction status.
    
    Since interaction checking is only done when any gizmo is enabled, if
    a gizmo is disabled while it's being interacted with and then destroyed,
    no other gizmo can be interacted with. A new check is added in
    UpdateInteracting for gizmoInteracting and the left mouse button is not
    held. This ensures that as long as there's one active gizmo, interaction
    checks are made.

--- src/scripts/gizmo_interaction_fix.cs
+++ src/scripts/gizmo_interaction_fix.cs
@@ -27,61 +27,91 @@ public static class GizmoInteractionFix
 
     private static void UpdateInteracting(GizmoRender gizmoRender, GizmoRender.MOVETYPE startMoveType)
     {
-        if (startMoveType == gizmoRender.beSelectedType)
-            return;
-
-        if (startMoveType is GizmoRender.MOVETYPE.NONE && NInput.GetMouseButton(0))
-        {
-            gizmoInteracting = true;
-            interactingGizmoID = gizmoRender.GetInstanceID();
-        }
-        else if (gizmoRender.beSelectedType is GizmoRender.MOVETYPE.NONE)
+        if (gizmoInteracting && !NInput.GetMouseButton(0))
         {
             gizmoInteracting = false;
             interactingGizmoID = -1;
         }
+        else if (startMoveType != gizmoRender.beSelectedType && startMoveType == GizmoRender.MOVETYPE.NONE)
+        {
+            gizmoInteracting = true;
+            interactingGizmoID = gizmoRender.GetInstanceID();
+        }
+    }
+
+    private static void VisibleUpdateInteracting(GizmoRender gizmoRender)
+    {
+        if (!gizmoInteracting || !NInput.GetMouseButton(0))
+            return;
+
+        if (gizmoRender.GetInstanceID() != interactingGizmoID)
+            return;
+
+        gizmoInteracting = false;
+        interactingGizmoID = -1;
     }
 
     [HarmonyPatch(typeof(GizmoRender), nameof(GizmoRender.OnRenderObject))]
     [HarmonyTranspiler]
     private static IEnumerable<CodeInstruction> OnRenderObjectTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator)
     {
-        var matcher = new CodeMatcher(instructions, ilGenerator)
-            .MatchEndForward(
-                new CodeMatch(OpCodes.Ldarg_0),
-                new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(GizmoRender), nameof(GizmoRender.beSelectedType))),
-                new CodeMatch(OpCodes.Stloc_0),
-                new CodeMatch(OpCodes.Ldarg_0));
-
-        var originalPosition = matcher.Pos;
+        var matcher = new CodeMatcher(instructions, ilGenerator);
 
         matcher
+
+            // Find end of if (!Visible) block
+            .MatchEndForward(
+                new CodeMatch(OpCodes.Ldarg_0),
+                new CodeMatch(OpCodes.Ldc_I4_0),
+                new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(GizmoRender), nameof(GizmoRender.beSelectedType))))
+            .Advance(1)
+
+            // Disable gizmo interacting if the non-visible gizmo is the interacting gizmo
             .InsertAndAdvance(
-                new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(GizmoInteractionFix), nameof(gizmoInteracting))))
-            .InsertBranchAndAdvance(OpCodes.Brfalse, originalPosition + 1)
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GizmoInteractionFix), nameof(VisibleUpdateInteracting))))
+
+            // Find end of initializing the mOVETYPE variable
+            .MatchEndForward(
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(GizmoRender), nameof(GizmoRender.beSelectedType))),
+                new CodeInstruction(OpCodes.Stloc_0))
+            .Advance(1)
+            .CreateLabel(out var originalPosition)
+
+            // Skip checking interacting gizmo and ID if no gizmo is being interacted with
+            .InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(GizmoInteractionFix), nameof(gizmoInteracting))),
+                new CodeInstruction(OpCodes.Brfalse, originalPosition))
+
+            // Add gizmo ID and interacting gizmo ID to the stack
             .InsertAndAdvance(
                 new CodeInstruction(OpCodes.Ldarg_0),
                 new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Object), nameof(Object.GetInstanceID))),
                 new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(GizmoInteractionFix), nameof(interactingGizmoID))));
 
-        var branchPosition = matcher.Pos;
+        var gizmoIDCheckPosition = matcher.Pos;
 
-        var renderGizmoEndPosition = matcher
-            .MatchStartForward(
+        matcher
+            .End()
+
+            // Find the "end" of the method and make a label to jump to
+            .MatchStartBackwards(
                 new CodeMatch(OpCodes.Ldloc_0),
                 new CodeMatch(OpCodes.Brtrue),
                 new CodeMatch(OpCodes.Ldarg_0),
                 new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(GizmoRender), nameof(GizmoRender.beSelectedType))))
-            .Pos;
+            .CreateLabel(out var renderGizmoEndLabel)
 
-        matcher
+            // Go back to id check position
             .Start()
-            .Advance(branchPosition)
-            .InsertBranchAndAdvance(OpCodes.Bne_Un, renderGizmoEndPosition)
-            .MatchEndForward(
-                new CodeMatch(OpCodes.Ldarg_0),
-                new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(GizmoRender), nameof(GizmoRender.RenderGizmos))),
-                new CodeMatch(OpCodes.Ret))
+            .Advance(gizmoIDCheckPosition)
+
+            // Branch to end label if the gizmo id does not match the interacting gizmo ID
+            .InsertAndAdvance(new CodeInstruction(OpCodes.Bne_Un, renderGizmoEndLabel))
+            .End()
+
+            // Update interaction status at the end
             .InsertAndAdvance(
                 new CodeInstruction(OpCodes.Ldarg_0),
                 new CodeInstruction(OpCodes.Ldloc_0),
